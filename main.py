0adfbc02f2e79c5de5ca1635a80295d8e5dc8fe3
erick.marinho <erickgaves@gmail.com>
2025-05-19 18:58:12 -0300

    instalando langgraph

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI(
     title="OmniView",
     description="OmniView is a web application that allows you to view your home from anywhere.",
     version="0.1.0",
 )
+
 
 @app.get("/")
 def read_root():

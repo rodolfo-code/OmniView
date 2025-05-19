f5bec94109d942c3bdb9aa932760c51db45302d4
erick.marinho <erickgaves@gmail.com>
2025-05-19 18:51:05 -0300

    iniciando projeto instalando fastapi

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="OmniView",
+    description="OmniView is a web application that allows you to view your home from anywhere.",
+    version="0.1.0",
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
